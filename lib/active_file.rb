901a6024047010e377781024cee98e2aac2bfaff
Suellen Aline de Paula <suellen.alinep@gmail.com>
2018-03-12 20:41:10 +0000

    save find

---
+++ lib/active_file.rb
@@ -0,0 +1,50 @@
+# coding: utf-8
+require "FileUtils"
+
+module ActiveFile
+	def save
+		@new_record = false
+
+		File.open("db/revistas/#{@id}.yml", "w") do |file|
+			file.puts serialize
+		end
+	end
+	
+	def destroy
+		unless @destroyed or new_record
+			@destrouyed = true
+			FileUtils.rm "db/revistas/#{@id}.yml"
+		end
+	end
+
+	module ClassMethods
+		def find(id)
+			raise DocumentNotFound,
+				"Arquivos db/revistas/#{id} não encontrado.", caller
+				unless File.exists?("db/revistas/#{@id}.yml")
+			YAML.load File.open("db/revistas/#{@id}.yml", "r")
+		end
+
+		#def next_id
+		#	Dir.glob("db/revistas/*.yml").size + 1
+		#end
+		def field(name)
+			@fields ||= []
+			@fields << name
+		end
+	end
+	
+	def self.included(base)
+		base.extend ClassMethods
+	end
+
+	private
+	
+	def serialize
+		YAML.dump self
+	end
+end
+	
+
+
+
--- lib/revista.rb
+++ lib/revista.rb
@@ -1,7 +1,30 @@
 class Revista
-    attr_reader :titulo
+	attr_reader :titulo, :id
+	attr_accessor :valor
 
-    def initialize(titulo)
-	@titulo = titulo
-    end
+	def initialize(titulo, valor)
+		@titulo = titulo
+		@valor = valor
+		@id = self.class.next_id
+	end
+	
+	def save
+		File.open("./db/revistas/#{@id}.yml", "w") do |file|
+			file.puts serialize
+		end
+	end
+
+	def self.find(id)
+		YAML.load File.open("./db/revistas/#{@id}.yml","r")
+	end
+
+	private
+	
+	def serialize
+		YAML.dump self
+	end
+
+	def self.next_id
+		Dir.glob("./db/revistas/*.yml").size + 1
+	end
 end
