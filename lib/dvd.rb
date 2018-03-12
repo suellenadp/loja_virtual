4c46e7d546ae7880558cd37bb71847febe627fa4
Suellen Aline de Paula <suellen.alinep@gmail.com>
2018-03-11 23:01:22 -0300

    parte self

--- lib/dvd.rb
+++ lib/dvd.rb
@@ -1,5 +1,23 @@
 # coding: utf-8
 class DVD < Midia
+
+	include FormatadorMoeda
+
+	attr_reader :titulo
+
+	def self.formata_moeda(*variaveis_e_metodos)
+		variaveis_e_metodos.each do |name|
+			define_method("#{name}_formatado") do
+				valor = respond_to?(name) ? send(name) : instance_variable_get("@#{name}")
+				"R$ #{valor}"
+			end
+		end
+	end	
+
+
+	formata_moeda :valor_com_desconto, :valor
+
+
 	def initialize(titulo, valor, categoria)
 		super()
 		@titulo = titulo	
---
+++ lib/loja_virtualpc.rb
@@ -0,0 +1,12 @@
+require File.expand_path('/home/suellen/Documentos/Ruby/loja_virtual/lib/formatador_moeda')
+require File.expand_path('/home/suellen/Documentos/Ruby/loja_virtual/lib/midia')
+require File.expand_path('/home/suellen/Documentos/Ruby/loja_virtual/lib/dvd')
+require File.expand_path('/home/suellen/Documentos/Ruby/loja_virtual/lib/livro')
+require File.expand_path('/home/suellen/Documentos/Ruby/loja_virtual/lib/cd')
+require File.expand_path('/home/suellen/Documentos/Ruby/loja_virtual/lib/revista')
+require File.expand_path('/home/suellen/Documentos/Ruby/loja_virtual/lib/biblioteca')
+require File.expand_path('/home/suellen/Documentos/Ruby/loja_virtual/lib/relatorio')
+require File.expand_path('/home/suellen/Documentos/Ruby/loja_virtual/lib/banco_de_arquivos')
+require File.expand_path('/home/suellen/Documentos/Ruby/loja_virtual/lib/VendaFacil')
+
+
