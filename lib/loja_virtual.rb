2d744f945ba05d1571bd2ca3ca17231f0929e2ea
diogoab <diogo.alves.barbosa@gmail.com>
2018-03-13 21:41:10 -0300

    add include active_file in the loja_virtual.rb

--- lib/loja_virtual.rb
+++ lib/loja_virtual.rb
@@ -8,5 +8,6 @@ require File.expand_path('/home/suellen_alinep/loja_virtual/lib/biblioteca')
 require File.expand_path('/home/suellen_alinep/loja_virtual/lib/relatorio')
 require File.expand_path('/home/suellen_alinep/loja_virtual/lib/banco_de_arquivos')
 require File.expand_path('/home/suellen_alinep/loja_virtual/lib/VendaFacil')
+require File.expand_path('/home/suellen_alinep/loja_virtual/lib/active_file')
 
 
